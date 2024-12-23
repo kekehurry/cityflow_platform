56babf43de774c36057b1ed357f42908b460dea7
kekehurry <arhukai1111@gmail.com>
2024-12-24 01:39:17 +0800

    fix file exists error

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 # Pull the required images
-# docker pull gboeing/osmnx
-# docker pull python:3-slim
+docker pull gboeing/osmnx
+docker pull python:3-slim
 
 docker network ls | grep -q cityflow || docker network create cityflow
 
-sed -i '' "s/EXECUTOR_USER=.*/EXECUTOR_USER=$(id -u):$(id -g)/" .env
+# change user to current user
+sed -i "s/EXECUTOR_USER=.*/EXECUTOR_USER=$(id -u):$(id -g)/" .env
+sed -i "s/user:.*/user: '$(id -u):$(id -g)'/g" docker-compose.yml
 
 sudo usermod -aG docker $USER
 
-sudo chown $(id -u):$(id -g) cityflow_database
-sudo chown $(id -u):$(id -g) cityflow_executor
-sudo chown $(id -u):$(id -g) /var/run/docker.sock
+# change the owner of the cityflow_database and cityflow_executor
+sudo chown -R $(id -u):$(id -g) cityflow_database
+sudo chown -R $(id -u):$(id -g) cityflow_executor
+sudo chown -R $(id -u):$(id -g) /var/run/docker.sock
 
 # reload docker daemon and restart docker
 sudo systemctl daemon-reload
